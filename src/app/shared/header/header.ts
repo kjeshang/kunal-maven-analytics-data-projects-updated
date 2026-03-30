65b65e0b450b2234609f535a04d40fb2adc1d71c
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-03-29 17:47:52 -0700

    Create header
    Create mobile responsive header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
