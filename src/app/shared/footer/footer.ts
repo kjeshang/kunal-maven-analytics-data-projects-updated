66875b4209d2f31d7d5ba1ccd15ae6974d724f9d
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-03-29 18:46:50 -0700

    Adjust footer
    Adjust footer

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatDividerModule} from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatDividerModule],
+  imports: [MatDividerModule, MatTooltipModule, MatIconModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
