43be42aca179bb14e7404e5e6402254925d89b56
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-03-31 10:07:52 -0700

    Created general mat-dialog layout

--- src/app/components/viewdialog/viewdialog.ts
+++ src/app/components/viewdialog/viewdialog.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { JupyterAnalysis } from '../../state/models';
 
 @Component({
   selector: 'app-viewdialog',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './viewdialog.html',
   styleUrl: './viewdialog.css',
 })
 export class Viewdialog {
   readonly dialogRef: MatDialogRef<Viewdialog> = inject(MatDialogRef<Viewdialog>);
-  data = inject(MAT_DIALOG_DATA);
+  data: JupyterAnalysis = inject(MAT_DIALOG_DATA);
 }
