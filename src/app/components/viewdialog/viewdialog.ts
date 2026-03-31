11a6591f6495214a4c7ca4827eefe2df62bab094
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-03-30 23:27:11 -0700

    Start customize view
    Start customize view

--- src/app/components/viewdialog/viewdialog.ts
+++ src/app/components/viewdialog/viewdialog.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-viewdialog',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatButtonModule],
   templateUrl: './viewdialog.html',
   styleUrl: './viewdialog.css',
 })
-export class Viewdialog {}
+export class Viewdialog {
+  readonly dialogRef: MatDialogRef<Viewdialog> = inject(MatDialogRef<Viewdialog>);
+  data = inject(MAT_DIALOG_DATA);
+}
