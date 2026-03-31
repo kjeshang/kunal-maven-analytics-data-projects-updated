6565e93e734bb6464318e7369128dd15d32bd67e
kjeshang <77552335+kjeshang@users.noreply.github.com>
2026-03-30 22:50:47 -0700

    Restructuring
    Restructuring

--- src/app/components/customcard/customcard.ts
+++ src/app/components/customcard/customcard.ts
@@ -3,26 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { JupyterAnalysis } from '../../state/models';
-import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { Viewdialog } from '../viewdialog/viewdialog';
 
 @Component({
   selector: 'app-customcard',
-  imports: [MatButtonModule, MatCardModule, MatDividerModule, MatDialogModule],
+  imports: [MatButtonModule, MatCardModule, MatDividerModule],
   templateUrl: './customcard.html',
   styleUrl: './customcard.css',
 })
 export class Customcard {
   @Input() item!: JupyterAnalysis;
-  readonly dialog: MatDialog = inject(MatDialog);
-
-  /**
-   * Click card to open dialog window.
-   */
-  openAnalysisWindow(): void {
-    const dialogRef: MatDialogRef<Viewdialog, any> = this.dialog.open(Viewdialog, {
-      width:"99vw",
-      height:"95vh"
-    });
-  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,17 +1,34 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FeatureContainer } from '../../components/feature-container/feature-container';
 import { JupyterAnalysisStore } from '../../state/jupyterAnalysis.store';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Customcard } from '../../components/customcard/customcard';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { Viewdialog } from '../../components/viewdialog/viewdialog';
+import { JupyterAnalysis } from '../../state/models';
 
 @Component({
   selector: 'app-home',
-  imports: [FeatureContainer, MatProgressSpinnerModule, Customcard],
+  imports: [FeatureContainer, MatProgressSpinnerModule, Customcard, MatDialogModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
 
   jupyterAnalysisStore = inject(JupyterAnalysisStore);
+
+  readonly dialog: MatDialog = inject(MatDialog);
+
+  /**
+   * Click card to open dialog window.
+   */
+  openAnalysisWindow(item: JupyterAnalysis): void {
+    this.jupyterAnalysisStore.updateSelectedJupyterAnalysis(item);
+    const dialogRef: MatDialogRef<Viewdialog, any> = this.dialog.open(Viewdialog, {
+      width:"99vw",
+      height:"95vh",
+      data: item,
+    });
+  }
 
 }
